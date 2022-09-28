c90e8945e980c540afc49c0c11ccd9f6ca8b9994
Claudia Beresford <claudiaberesford@gmail.com>
2022-09-28 15:22:24 +0100

    Change header title

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`${siteConfig.title}`}
       description="Declarative, lightweight, fast.">
       <HomepageHeader />
       <main>
