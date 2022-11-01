69529bcaa8312232a5744563abe02a668dd634f5
Claudia Beresford <claudiaberesford@gmail.com>
2022-11-01 13:14:40 +0000

    Add map example to capmvm yamls

--- hack/capmvm.sh
+++ hack/capmvm.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
 
 TEMPDIR=$(mktemp -d)
-CTY_VERSION="v0.0.8"
+CTY_VERSION="v0.0.11"
 
 cleanup() {
 	popd &> /dev/null || exit 1
 	rm -rf "$TEMPDIR"
 }
 
-get_crd() {
+crd_url() {
 	local object="$1"
 
-	wget -q "https://raw.githubusercontent.com/weaveworks-liquidmetal/cluster-api-provider-microvm/main/config/crd/bases/infrastructure.cluster.x-k8s.io_$object.yaml" \
-		-O "$TEMPDIR/$object.yaml"
+	echo "https://raw.githubusercontent.com/weaveworks-liquidmetal/cluster-api-provider-microvm/main/config/crd/bases/infrastructure.cluster.x-k8s.io_$object.yaml"
 }
 
 download_cty() {
-	curl -sL "https://github.com/Skarlso/crd-to-sample-yaml/releases/download/$CTY_VERSION/crd-to-yaml_linux_amd64.tar.gz" \
+	curl -sL "https://github.com/Skarlso/crd-to-sample-yaml/releases/download/$CTY_VERSION/cty_linux_amd64.tar.gz" \
 		| tar xz
 }
 
 generate_yaml() {
 	local object="$1"
 
-	./crd-to-yaml generate -c "$object.yaml"
+	./cty generate --stdout --url "$(crd_url "$object")"
 }
 
 add_yaml_to_page() {
@@ -33,8 +32,15 @@ add_yaml_to_page() {
 
 	echo "$input" | awk -v n="$count" \
 		-v patt='```yaml' \
-		-v content="$(cat "$TEMPDIR/${object}_v1alpha1.yaml")" \
+		-v content="$(generate_yaml "$object")" \
 		'{print} $0 ~ patt && ++count == n {print content}'
+}
+
+edit_maps() {
+	local key="$1"
+	local file="$2"
+
+	sed -i "s|\($key:\)\(.*\)|\1 {string: \"string\"}|" "$file"
 }
 
 trap cleanup EXIT
@@ -43,22 +49,17 @@ START=$(pwd)
 
 pushd "$TEMPDIR" || exit 1
 
-# fetch the CRDs from github
-get_crd "microvmclusters"
-get_crd "microvmmachines"
-get_crd "microvmmachinetemplates"
-
-# generate some nice yamls
 download_cty
-generate_yaml "microvmclusters"
-generate_yaml "microvmmachines"
-generate_yaml "microvmmachinetemplates"
 
-# add those yamls to the site doc
-one=$(add_yaml_to_page "$(cat "$START/docs/guides/capmvm.md")" 1 "MicrovmCluster")
-two=$(add_yaml_to_page "$one" 2 "MicrovmMachine")
-add_yaml_to_page "$two" 3 "MicrovmMachineTemplate" > three.yaml
+# add generated yamls to the site doc
+one=$(add_yaml_to_page "$(cat "$START/docs/guides/capmvm.md")" 1 "microvmclusters")
+two=$(add_yaml_to_page "$one" 2 "microvmmachines")
+add_yaml_to_page "$two" 3 "microvmmachinetemplates" > three.yaml
 mv three.yaml "$START/docs/guides/capmvm.md"
 
 ## update to make map[string]string clearer
-## TODO
+# this is just a hacky stop-gap until the feature is better in cty
+edit_maps kernelCmdline "$START/docs/guides/capmvm.md"
+edit_maps annotations "$START/docs/guides/capmvm.md"
+edit_maps labels "$START/docs/guides/capmvm.md"
+edit_maps attributes "$START/docs/guides/capmvm.md"
